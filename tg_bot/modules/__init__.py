b297d25faf66ba42e20c0e8f1c100afbfb821daf
waydk <waydk.main@gmail.com>
2021-06-27 13:10:55 +0300

    0.0.5: add ban and unban commands

--- tg_bot/modules/__init__.py
+++ tg_bot/modules/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
 
-from .antiflood import command_ro, command_un_ro
+from .antiflood import command_ro, command_un_ro, command_ban, command_un_ban
 from .errors import errors_handler
 from .start import start_command
 from ..utils.filters import AdminFilter
@@ -11,3 +11,5 @@ def setup(dp: Dispatcher):
     dp.register_message_handler(start_command, commands="start")
     dp.register_message_handler(command_ro, AdminFilter(), commands="ro", commands_prefix='!/')
     dp.register_message_handler(command_un_ro, AdminFilter(), commands="un_ro", commands_prefix='!/')
+    dp.register_message_handler(command_ban, AdminFilter(), commands="ban", commands_prefix='!/')
+    dp.register_message_handler(command_un_ban, AdminFilter(), commands="un_ban", commands_prefix='!/')
--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -33,7 +33,7 @@ async def command_ro(message: types.Message):
     await service_message.delete()
 
 
-async def command_un_ro(message):
+async def command_un_ro(message: types.Message):
     """
     A command that removes restrictions from the user
     """
@@ -48,3 +48,35 @@ async def command_un_ro(message):
     except aiogram.utils.exceptions.BadRequest as err:
         await message.answer(f"Error! - <code>{err.args}</code>")
         return
+
+
+async def command_ban(message: types.Message):
+    """
+    The command allows you to ban a user
+    """
+    try:
+        member = message.reply_to_message.from_user
+    except AttributeError:
+        await message.answer("<b>The message must be forwarded!</b>")
+        return
+    try:
+        await message.chat.kick(user_id=member.id)
+    except BadRequest:
+        await message.answer(f"User {member.id} - admin")
+
+    service_message = await message.answer("The message will be deleted after 5 seconds.")
+    await asyncio.sleep(5)
+    await message.reply_to_message.delete()
+    await message.delete()
+    await service_message.delete()
+
+
+async def command_un_ban(message: types.Message):
+    try:
+        member = message.reply_to_message.from_user
+    except AttributeError:
+        await message.answer("<b>The message must be forwarded!</b>")
+        return
+    await message.chat.unban(user_id=member.id)
+    await message.answer(f"{member.full_name} unbanned")
+
